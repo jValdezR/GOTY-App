43aad218911caf67d8fc8a92cd0c5bdda3c01f57
jValdez <jdv_rodriguez@hotmail.com>
2020-09-14 08:27:02 -0500

    estructura del proyecto terminada

---
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-barra-horizontal',
+  templateUrl: './grafico-barra-horizontal.component.html',
+  styleUrls: ['./grafico-barra-horizontal.component.css']
+})
+export class GraficoBarraHorizontalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
