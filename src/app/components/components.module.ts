2f0f287b2f73f5b22ffca1f2763c2b7c10fbf1a0
jValdez <jdv_rodriguez@hotmail.com>
2020-09-14 08:55:33 -0500

    Grafica en tiempo real

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/grafico-barra-horizontal.component'
 import { RouterModule } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
@@ -12,10 +13,13 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BrowserAnimationsModule,
+    NgxChartsModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    GraficoBarraHorizontalComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent implements OnInit {
+export class GraficoBarraHorizontalComponent implements OnDestroy{
 
-  constructor() { }
+  results: any[] = [
+    {
+      name: 'Juego 1',
+      value: 20
+    },
+    {
+      name: 'Juego 2',
+      value: 25
+    },
+    {
+      name: 'Juego 3',
+      value: 15
+    },
+    {
+      name: 'Juego 4',
+      value: 30
+    }
+  ];
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Juegos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votos';
 
-  ngOnInit(): void {
+  colorScheme = 'nightLights';
+
+  intervalo;
+  constructor() {
+
+    this.intervalo = setInterval(() => {
+      console.log('tick');
+      const newResults = [...this.results];
+      // tslint:disable-next-line: forin
+      for(let i in newResults){
+        newResults[i].value = Math.round(Math.random() * 500);
+      }
+      this.results = [...newResults];
+    }, 1500);
   }
+
+  ngOnDestroy(){
+    clearInterval(this.intervalo);
+  }
+
 
 }
